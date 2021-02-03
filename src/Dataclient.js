42699e7af2fab33d47512cc0a14fc0e981d7c544
Craig Hopgood <searaig@gmail.com>
2021-02-03 16:21:19 -0500

    initial import

---
+++ src/Dataclient.js
@@ -0,0 +1,34 @@
+import {io} from "socket.io-client"
+
+const Dataclient = port => {
+    const url = `ws://localhost:${port}`
+    const socket = io(url)
+
+    socket.on("connect", () => console.log(`connect to socket via ${url}`))
+
+    return {
+        push: data => {
+            socket
+                .compress(true)
+                .emit("data", data)
+            return data
+        },
+
+        pull: fn => {
+            socket
+                .on("data", data => fn(data))
+        },
+
+        sustain: data => {
+            socket
+                .compress(true)
+                .emit("sustain", data)
+        },
+
+        release: data => {
+            socket.emit("release", data)
+        }
+    }
+}
+
+export default Dataclient
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Dataclient from './Dataclient'
+
+export  {
+    Dataclient,
+}
